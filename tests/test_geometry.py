d1785684347e22e573987834fb062a93ccd3b178
axonde <alex.chpak@gmail.com>
2024-11-10 19:07:26 +0300

    corrected geometric test to A-A-A structure

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -5,22 +5,37 @@ import triangle
 
 class TestGeometry(TestCase):
     def test_circle_area(self):
-        self.assertEqual(circle.area(5), 78.53981633974483)
+        # arrange
+        radius = 5
+        # act
+        result = circle.area(radius)
+        # assert
+        self.assertEqual(result, 78.53981633974483)
 
     def test_circle_perimiter(self):
-        self.assertEqual(circle.perimeter(17), 106.81415022205297)
+        radius = 17
+        result = circle.perimeter(radius)
+        self.assertEqual(result, 106.81415022205297)
 
     def test_square_area(self):
-        self.assertEqual(square.area(12), 144)
+        side = 12
+        result = square.area(side)
+        self.assertEqual(result, 144)
 
     def test_square_perimiter(self):
-        self.assertEqual(square.perimeter(11), 44)
+        side = 11
+        result = square.perimeter(side)
+        self.assertEqual(result, 44)
     
     def test_triangle_area(self):
-        self.assertEqual(triangle.area(3, 4, 5), 6)
+        sides = [3, 4, 5]
+        result = triangle.area(*sides)
+        self.assertEqual(result, 6)
 
     def test_triangle_perimiter(self):
-        self.assertEqual(triangle.perimeter(5, 9, 1), 15)
+        sides = [5, 9, 1]
+        result = triangle.perimeter(*sides)
+        self.assertEqual(result, 15)
 
 if __name__ == '__main__':
     main()
